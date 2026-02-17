811ef7a15ad67686c01afe39ba3d9e627125ff93
Mahesh <pakamaheshkumar65@gmail.com>
2026-02-16 21:14:50 -0600

    Fixed issue in mobile

--- js/main.js
+++ js/main.js
@@ -97,28 +97,27 @@
 	
 
 	var carousel = function() {
-		$('.home-slider').owlCarousel({
-	    loop:true,
-	    autoplay: true,
-	    margin:0,
-	    animateOut: 'fadeOut',
-	    animateIn: 'fadeIn',
-	    nav:false,
-	    autoplayHoverPause: false,
-	    items: 1,
-	    navText : ["<span class='ion-md-arrow-back'></span>","<span class='ion-chevron-right'></span>"],
-	    responsive:{
-	      0:{
-	        items:1
-	      },
-	      600:{
-	        items:1
-	      },
-	      1000:{
-	        items:1
-	      }
-	    }
-		});
+		// Only init Owl Carousel on desktop. On mobile it blocks touch/scroll (touch-action: none).
+		if ($(window).width() > 768) {
+			$('.home-slider').owlCarousel({
+		    loop: true,
+		    autoplay: true,
+		    margin: 0,
+		    animateOut: 'fadeOut',
+		    animateIn: 'fadeIn',
+		    nav: false,
+		    autoplayHoverPause: false,
+		    items: 1,
+		    navText: ["<span class='ion-md-arrow-back'></span>","<span class='ion-chevron-right'></span>"],
+		    responsive: {
+		      0: { items: 1 },
+		      600: { items: 1 },
+		      1000: { items: 1 }
+		    }
+			});
+		} else {
+			$('.home-slider').addClass('mobile-hero-static');
+		}
 	};
 	carousel();
 
